e0d5947f7b44afb986788f6d3aecee4dee2cda24
lecharri <letivector@gmail.com>
2019-03-12 19:22:48 +0100

    Search series by name

--- MySeries/Series/View/SeriesViewController.swift
+++ MySeries/Series/View/SeriesViewController.swift
@@ -19,6 +19,11 @@ class SeriesViewController: ParentViewController {
     let celID = "seriesCell"
     var series = [CDSerie]()
     
+    //Search Bar
+    var searchController: UISearchController?
+    var filteredSeries = [CDSerie]()
+    var activeSearchBar = false
+    
     let presenter: SeriesPresenterProtocol
     
     init(presenter: SeriesPresenterProtocol) {
@@ -37,9 +42,20 @@ extension SeriesViewController: SeriesViewControllerProtocol {
         super.viewDidLoad()
         
         tableView.register(UINib(nibName: "SerieListTableViewCell", bundle: nil), forCellReuseIdentifier: celID)
+        tableView.rowHeight = 50
         tableView.tableFooterView = UIView() //Clear extra lines
         
+        configureSearchBar()
+        
         series = CoreDataManager().fetchSeries()
+    }
+    
+    private func configureSearchBar() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController?.searchBar.delegate = self
+        searchController?.obscuresBackgroundDuringPresentation = false
+        searchController?.searchBar.placeholder = "Nombre de la serie a buscar"
+        tableView.tableHeaderView = searchController?.searchBar
     }
     
     func getViewController() -> SeriesViewController {
@@ -49,20 +65,49 @@ extension SeriesViewController: SeriesViewControllerProtocol {
 
 extension SeriesViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return series.count
+        return !activeSearchBar ? series.count : filteredSeries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: celID, for: indexPath) as! SerieListTableViewCell
         
-        let serie = series[indexPath.row]
-        cell.lblName.text = serie.name
-        cell.idSerie = Int(serie.id)
+        var serie: CDSerie?
+        
+        if !activeSearchBar {
+            serie = series[indexPath.row]
+        } else {
+            serie = filteredSeries[indexPath.row]
+        }
+        
+        cell.lblName.text = serie!.name
+        cell.idSerie = Int(serie!.id)
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         presenter.navigateToDetail(serie: series[indexPath.row])
+    }
+}
+
+extension SeriesViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if !searchText.isEmpty {
+            filteredSeries = series.filter { ($0.name?.lowercased().contains(searchText.lowercased()))! }
+            activeSearchBar = true
+        } else {
+            filteredSeries.removeAll(keepingCapacity: true)
+            activeSearchBar = false
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        filteredSeries.removeAll(keepingCapacity: true)
+        activeSearchBar = false
+        tableView.reloadData()
     }
 }
