4f127f72328f72ce9d0e30bf9e20eb2541aeac3f
Leticia Echarri <lecharri@minsait.com>
2019-05-28 17:47:37 +0200

    Fill cast with data

--- MySeries/Sections/Cast/View/CastViewController.swift
+++ MySeries/Sections/Cast/View/CastViewController.swift
@@ -15,9 +15,22 @@ class CastViewController: ParentViewController {
     
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
+            collectionView.delegate = self
+            collectionView.dataSource = self
+            
+            collectionView.register(CastCollectionViewCell.nib, forCellWithReuseIdentifier: CastCollectionViewCell.cellID)
+            
             collectionView.layer.borderWidth = 1
             collectionView.layer.borderColor = UIColor.white.cgColor
             collectionView.layer.cornerRadius = 10
+            
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            layout.itemSize = CGSize(width: 110, height: 173)
+            layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+            layout.minimumLineSpacing = 5
+            layout.minimumInteritemSpacing = 0
+            collectionView.setCollectionViewLayout(layout, animated: true)
         }
     }
     
@@ -46,4 +59,24 @@ class CastViewController: ParentViewController {
 
 extension CastViewController: CastViewControllerProtocol {
     
+}
+
+extension CastViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cast.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CastCollectionViewCell.cellID, for: indexPath) as? CastCollectionViewCell
+        
+        if cell != nil {
+            
+            let currentCast = cast[indexPath.row]
+            cell?.set(cast: currentCast)
+            
+            return cell!
+        }
+        
+        return UICollectionViewCell()
+    }
 }
